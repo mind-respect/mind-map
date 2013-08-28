e2df339af8d891af227ce8fa4f00e00d7c8929c5
Vincent Blouin <vincent.blouin@gmail.com>
2013-08-28 16:05:46 -0400

    progress upload images for vertex

--- src/main/webapp/js/triple_brain/graph/vertex/triple_brain.image_menu.js
+++ src/main/webapp/js/triple_brain/graph/vertex/triple_brain.image_menu.js
@@ -8,16 +8,16 @@ define([
 ],
     function ($, GraphElementMenu, GraphUi) {
         var api = {};
-        api.ofVertex = function(vertex){
+        api.ofVertex = function (vertex) {
             return new ImageMenu(
                 vertex
             );
         };
         return api;
-        function ImageMenu(vertex){
+        function ImageMenu(vertex) {
             var self = this;
             var html = $("<div>");
-            this.build = function(){
+            this.build = function () {
                 GraphUi.addHtml(html);
                 html.append(form());
                 GraphElementMenu.makeForMenuContentAndGraphElement(
@@ -27,21 +27,86 @@ define([
                 return self;
             };
 
-            function form(){
+            function form() {
                 html.data(
                     "vertex",
                     vertex
                 );
-                var form = $(
+                return $(
                     "<form>"
+                ).prop(
+                    "action",
+                    vertex.getUri() + "/image"
+                ).prop(
+                    "method",
+                    "post"
+                ).prop(
+                    "enctype",
+                    "multipart/form-data"
                 ).append(
                     uploadLabel()
+                ).append(
+                    fileInput()
                 );
-
-                function uploadLabel(){
+                function uploadLabel() {
                     return $(
-                        "<label data-i18n=''>"
+                        "<label data-i18n='vertex.menu.image.select'>"
                     );
+                }
+
+                function fileInput() {
+                    return $(
+                        "<input>"
+                    ).prop(
+                        "type",
+                        "file"
+                    ).prop(
+                        "name",
+                        "images"
+                    ).prop(
+                        "multiple",
+                        "multiple"
+                    ).data(
+                        "vertex",
+                        vertex
+                    ).data(
+                        "formData",
+                        new FormData()
+                    ).on(
+                        "change",
+                        function () {
+                            var fileInput = $(this),
+                                formData = fileInput.data("formData");
+                            for (var i = 0; i < this.files.length; i++) {
+                                var file = this.files[i];
+                                if (!!file.type.match(/image.*/)) {
+                                    var reader = new FileReader();
+                                    reader.onloadend = function (event) {
+                                        showUploadedItem(
+                                            event.target.result,
+                                            file.fileName
+                                        );
+                                    };
+                                    reader.readAsDataURL(file);
+                                    formData.append("images[]", file);
+                                }
+                            }
+                            var vertex = fileInput.data("vertex");
+                            $.ajax({
+                                url:vertex.getUri() + "/image",
+                                type:"POST",
+                                data:formData,
+                                processData:false,
+                                contentType:false,
+                                success:function (res) {
+                                    console.log("success " + res);
+                                }
+                            });
+                        }
+                    );
+                    function showUploadedItem() {
+
+                    }
                 }
             }
         }
