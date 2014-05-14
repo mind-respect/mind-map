b45d6fa114d680fd78cea905148c781594ac569a
Vincent Blouin <vincent.blouin@gmail.com>
2014-05-14 07:43:37 -0400

    script minification in prod

---
+++ src/main/webapp/build-config.js
@@ -0,0 +1,6 @@
+({
+    baseUrl:"js",
+    mainConfigFile:"js/common-boot.js",
+    name: "mind_map-boot",
+    out: "js/mind-map-built.js"
+})
--- src/main/webapp/js/mind_map-boot.js
+++ src/main/webapp/js/mind_map-boot.js
@@ -4,8 +4,8 @@
 
 requirejs(
     [
-        'module/mind_map/vertices_list/triple_brain.module.vertices_list',
-        'module/mind_map/date_picker/triple_brain.module.date_picker',
+        '../module/mind_map/vertices_list/triple_brain.module.vertices_list',
+        '../module/mind_map/date_picker/triple_brain.module.date_picker',
         'triple_brain/triple_brain.ui.mind_map_starter'
     ], function(VerticesList, DatePicker, mindMapApp){
         mindMapApp.start();
