652d7ed9de9769dcddf51d5a5de7c04ef23627f5
Vincent Blouin <vincent.blouin@gmail.com>
2014-07-23 18:16:55 -0400

    performance

--- src/main/webapp/js/triple_brain/graph/triple_brain.grouped_relation.js
+++ src/main/webapp/js/triple_brain/graph/triple_brain.grouped_relation.js
@@ -5,34 +5,35 @@ define([
         "triple_brain.ui.graph"
     ], function (GraphUi) {
         var api = {};
-        api.withoutAnIdentification = function(){
+        api.withoutAnIdentification = function () {
             return new Object(undefined);
         };
         api.usingIdentification = function (identification) {
             return new Object(identification);
         };
+        Object.prototype.getIdentification = function () {
+            return this.identification;
+        };
+        Object.prototype.getVertices = function () {
+            return this.vertices;
+        };
+        Object.prototype.addVertex = function (vertex, edge) {
+            if (this.vertices[vertex.getUri()] === undefined) {
+                this.vertices[vertex.getUri()] = {};
+            }
+            this.vertices[
+                vertex.getUri()
+                ][
+                GraphUi.generateVertexHtmlId()
+                ] = {
+                vertex: vertex,
+                edge: edge
+            };
+        };
         return api;
         function Object(identification) {
-            var vertices = {};
-            this.getIdentification = function () {
-                return identification;
-            };
-            this.getVertices = function () {
-                return vertices;
-            };
-            this.addVertex = function (vertex, edge) {
-                if (vertices[vertex.getUri()] === undefined) {
-                    vertices[vertex.getUri()] = {};
-                }
-                vertices[
-                    vertex.getUri()
-                    ][
-                    GraphUi.generateVertexHtmlId()
-                    ] = {
-                    vertex: vertex,
-                    edge: edge
-                };
-            };
+            this.identification = identification;
+            this.vertices = {};
         }
     }
 );
