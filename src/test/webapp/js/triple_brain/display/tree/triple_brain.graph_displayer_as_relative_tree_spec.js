6a8a5a142754e9af32511e0f9d66259759c22da6
Vincent Blouin <vincent.blouin@gmail.com>
2014-11-17 09:46:52 -0500

    refactor

--- src/test/webapp/js/triple_brain/display/tree/triple_brain.graph_displayer_as_relative_tree_spec.js
+++ src/test/webapp/js/triple_brain/display/tree/triple_brain.graph_displayer_as_relative_tree_spec.js
@@ -4,11 +4,9 @@
 
 define([
     "triple_brain.graph_displayer_as_relative_tree",
-    "triple_brain.edge",
-    "triple_brain.vertex",
     "triple_brain.center_bubble",
     'test/webapp/js/test-scenarios'
-], function (GraphDisplayerAsRelativeTree, Edge, Vertex, CenterBubble, Scenarios) {
+], function (GraphDisplayerAsRelativeTree, CenterBubble, Scenarios) {
     "use strict";
     describe("graph_displayer_as_relative_tree_spec", function () {
         var bubble1;
@@ -27,18 +25,8 @@ define([
         });
 
         it("distributes new triples evenly to the right and left", function () {
-            var triple1 = Scenarios.getTriple(),
-                firstAddedEdge = GraphDisplayerAsRelativeTree.addEdgeAndVertex(
-                    bubble1,
-                    Edge.fromServerFormat(triple1.edge),
-                    Vertex.fromServerFormat(triple1.end_vertex)
-                ).edge(),
-                triple2 = Scenarios.getAnotherTriple(),
-                secondAddedEdge = GraphDisplayerAsRelativeTree.addEdgeAndVertex(
-                    bubble1,
-                    Edge.fromServerFormat(triple2.edge),
-                    Vertex.fromServerFormat(triple2.end_vertex)
-                ).edge();
+            var firstAddedEdge = Scenarios.addTriple(bubble1).edge(),
+                secondAddedEdge = Scenarios.addAnotherTriple(bubble1).edge();
             expect(
                 firstAddedEdge.isToTheLeft()
             ).toBeFalsy();
