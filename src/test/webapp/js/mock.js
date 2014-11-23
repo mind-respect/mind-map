1f700ef1f371639b9f265b08eb675f65a5fd6a60
Vincent Blouin <vincent.blouin@gmail.com>
2014-11-23 14:57:39 -0500

    test hidden relations container

--- src/test/webapp/js/mock.js
+++ src/test/webapp/js/mock.js
@@ -4,19 +4,14 @@
 
 define([
     "triple_brain.user",
-    "triple_brain.mind_map_info",
-    "triple_brain.graph_service"
-], function (UserService, MindMapInfo, GraphService) {
+    "triple_brain.mind_map_info"
+], function (UserService, MindMapInfo) {
     "use strict";
     var api = {};
     api.setCenterVertexUriInUrl = function(centerVertexUri){
         MindMapInfo._getCenterVertexUriInUrl = function(){
             return centerVertexUri;
         }
-    };
-    api.mockGetForCentralVertexUriOnce = function(returnValue){
-        GraphService.
-        getForCentralVertexUri
     };
     UserService.authenticatedUserInCache = function(){
         return {
