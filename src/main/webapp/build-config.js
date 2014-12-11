08e2a236751d733073f6e6a0e3686f5ee75eaa59
Vincent Blouin <vincent.blouin@gmail.com>
2014-12-11 07:57:17 -0500

    provide javascript source map

--- src/main/webapp/build-config.js
+++ src/main/webapp/build-config.js
@@ -6,5 +6,8 @@
     baseUrl:"js",
     mainConfigFile:"js/common-boot.js",
     name: "mind_map-boot",
-    out: "js/mind-map-built.js"
+    out: "js/mind-map-built.js",
+    generateSourceMaps: true,
+    preserveLicenseComments: false,
+    optimize: 'uglify2'
 })
