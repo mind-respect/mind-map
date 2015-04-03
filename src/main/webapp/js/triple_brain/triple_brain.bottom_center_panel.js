e92c2885c609f6d5169650b6ffb4690de12b79e2
Vincent Blouin <vincent.blouin@gmail.com>
2015-04-02 22:45:46 -0400

    about

--- src/main/webapp/js/triple_brain/triple_brain.bottom_center_panel.js
+++ src/main/webapp/js/triple_brain/triple_brain.bottom_center_panel.js
@@ -9,11 +9,19 @@ define([
     "use strict";
     var api = {};
     EventBus.subscribe('/event/ui/mind_map_info/is_view_only', function(){
-        if(!MindMapInfo.isCenterBubbleUriDefinedInUrl()){
-            getPanel().addClass("hidden");
+        if(MindMapInfo.isCenterBubbleUriDefinedInUrl()) {
+            getAboutLink().addClass("hidden");
+        }else{
+            getLegendContainer().addClass("hidden");
         }
     });
     return api;
+    function getLegendContainer(){
+        return getPanel().find(".legend-container");
+    }
+    function getAboutLink(){
+        return getPanel().find("a");
+    }
     function getPanel(){
         return $("#bottom-center-panel");
     }
