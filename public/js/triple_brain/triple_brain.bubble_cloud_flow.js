f746561b288deaa593ca2048597a8fb7aa952261
Vincent Blouin <vincent.blouin@gmail.com>
2015-10-10 22:40:25 -0400

    redirect if not much to show in cloud

--- public/js/triple_brain/triple_brain.bubble_cloud_flow.js
+++ public/js/triple_brain/triple_brain.bubble_cloud_flow.js
@@ -6,15 +6,27 @@ define([
     "jquery",
     "triple_brain.center_graph_element_service",
     "triple_brain.center_graph_elements",
-    "triple_brain.visited_elements_cloud"
-], function ($, CenterGraphElementService, CenterGraphElements, VisitedElementsCloud) {
+    "triple_brain.visited_elements_cloud",
+    "triple_brain.user_service"
+], function ($, CenterGraphElementService, CenterGraphElements, VisitedElementsCloud, UserService) {
     "use strict";
     var api = {};
     api.enter = function () {
         CenterGraphElementService.get(function (elements) {
             $("body").removeClass("hidden");
+            var centerGraphElements = CenterGraphElements.fromServerFormat(elements);
+            if(centerGraphElements === 0){
+                UserService.getDefaultVertexUri(function(uri){
+                    window.location = "?bubble=" + uri;
+                });
+                return;
+            }
+            if(centerGraphElements.length === 1){
+                window.location = "?bubble=" + centerGraphElements[0].getUri();
+                return;
+            }
             VisitedElementsCloud.buildFromElementsInContainer(
-                CenterGraphElements.fromServerFormat(elements),
+                centerGraphElements,
                 getContainer()
             );
         });
