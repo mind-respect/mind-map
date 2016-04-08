118e2db41b589be1cc189b7123d59648754f240d
Vincent Blouin <vincent.blouin@gmail.com>
2016-04-08 09:59:31 -0400

    shrink menu

--- public/js/triple_brain/triple_brain.header.js
+++ public/js/triple_brain/triple_brain.header.js
@@ -237,7 +237,7 @@ define([
         }
 
         function getConnectMenuButton(){
-            return $("#connect-menu-button")
+            return $("#connect-menu-button");
         }
         
         function getRegisterButton() {
